b083c9e91ed031c05fb98232ebc0ddd9e02bd0a3
ejcanoy <euan.james.canoy@gmail.com>
2023-07-25 09:54:33 -0700

    Created Node Class

---
+++ Node.js
@@ -0,0 +1,15 @@
+class BBSTNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    setLeft(left) {
+        this.left = left;
+    }
+
+    setRight(right) {
+        this.right = right;
+    }
+}
